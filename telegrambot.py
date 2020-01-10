76c791eb84b2886e04d43475180433ab67844289
AirMaks <rm1010@yandex.ru>
2020-01-10 17:45:04 +0300

    Readme

--- telegrambot.py
+++ telegrambot.py
@@ -1,31 +1,45 @@
 import telebot
-import env_vars
 from telebot import types
+import time
+import schedule
+import requests
 
-bot = telebot.TeleBot(env_vars.api_tok)
 
+bot_token = ''
+bot_chatID = ''
+bot = telebot.TeleBot(bot_token)
 markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-btn_yes = types.KeyboardButton('Да')
-btn_no = types.KeyboardButton('Нет')
+btn_yes = types.KeyboardButton('+')
+btn_no = types.KeyboardButton('-')
 btn_know = types.KeyboardButton('Пока не знаю')
 markup_menu.add(btn_yes, btn_no, btn_know)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет, сегодня тренировка, придешь?", reply_markup=markup_menu)
-
-
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    if message.text == 'Да':
-        bot.reply_to(message, 'Отлично! Не опаздываем!', reply_markup=markup_menu)
-    elif message.text == 'Нет':
-        bot.reply_to(message, 'Ну вот : ( В следующий раз обязательно приходи!')
-    elif message.text == 'Пока не знаю':
-        bot.reply_to(message, 'Хорошо, как узнаешь дай знать!', reply_markup=markup_menu)
-    else:
-        bot.reply_to(message, 'Неправльная команда.')
+    players_going_to_come = 0
+    if message.text == '+':
+        players_going_to_come += 1
+        bot.reply_to(message, str(players_going_to_come) + ' is going to come today.')
 
+
+def telegram_bot_sendtext(bot_message):
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
+
+
+def report():
+    my_message = "Всем привет, сегодня тренировка! Кто придет ставим плюсы! Кто нет - минусы."
+    telegram_bot_sendtext(my_message)
 
 bot.polling()
+# schedule.every(.1).minutes.do(report)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+    
+
+
+
